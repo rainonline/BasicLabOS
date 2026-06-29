89493cbaba661dc7afa83ae9efc28c9f10065a46
Rainy <haoyuhy@yeah.net>
2026-06-30 05:16:39 +0800

    Initial Commit

---
+++ BasicLabOS/BasicLabOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLabOSApp.swift
+//  BasicLabOS
+//
+//  Created by 郝雨 on 2026/6/30.
+//
+
+import SwiftUI
+
+@main
+struct BasicLabOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
